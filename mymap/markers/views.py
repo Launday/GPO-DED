0a2574cef88a6bfe2f58c3b4499f5c1485cdccee
Launday <daniilivanichev@yandex.ru>
2023-05-17 17:50:06 +0700

    Initial commit

---
+++ mymap/markers/views.py
@@ -0,0 +1,16 @@
+from django.views.generic.base import TemplateView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from markers.models import Marker
+from .serializers import MarkerSerializer
+
+
+class MarkerListAPIView(APIView):
+    def get(self, request):
+        markers = Marker.objects.all()
+        serializer = MarkerSerializer(markers, many=True)
+        return Response(serializer.data)
+
+
+class MarkersMapView(TemplateView):
+    template_name = "map.html"
